cbba98f2167a6c587a74f59e5d895e47d274c3a9
Huma_1165 <iftikharhuma922@gmail.com>
2025-03-09 23:12:22 -0700

    Parallel Sum Computation (parallelSumStatic) – Uses OpenMP with reduction(+:sum) and static scheduling for efficient parallel execution.

--- Static.c
+++ Static.c
@@ -8,3 +8,12 @@ void initializeArray(int arr[]) {
     for (int i = 0; i < N; i++)
         arr[i] = rand() % 100;
 }
+long long parallelSumStatic(int arr[]) {
+    long long sum = 0;
+    
+    #pragma omp parallel for schedule(static) reduction(+:sum)
+    for (int i = 0; i < N; i++)
+        sum += arr[i];
+
+    return sum;
+}
